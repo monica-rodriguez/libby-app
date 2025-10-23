860fd562be398afe38ccb82d887db5210ab31b7d
Monica-rodriguez <r.monicaba@gmail.com>
2025-10-22 21:32:01 -0400

    commits

--- src/main/java/com/starlight/app/repository/BooksRepo.java
+++ src/main/java/com/starlight/app/repository/BooksRepo.java
@@ -1,4 +1,20 @@
 package com.starlight.app.repository;
 
-public class BooksRepo {
+import com.starlight.app.model.entity.Books;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BooksRepo extends JpaRepository<Books, Long> {
+    //find by: full matches
+    List<Books> findByFirstNameIgnoreCase(String firstName);
+    List<Books> findByLastNameIgnoreCase(String lastName);
+    List<Books> findByBookTitleIgnoreCase(String bookTitle);
+    List<Books> findByBookGenreIgnoreCase(String bookGenre);
+
+    //find by: partial matches
+    List<Books> findByFirstNameContainingIgnoreCase(String firstName);
+    List<Books> findByLastNameContainingIgnoreCase(String lastName);
+    List<Books> findByBookTitleContainingIgnoreCase(String bookTitle);
+
 }
--- src/main/java/com/starlight/app/service/BookService.java
+++ src/main/java/com/starlight/app/service/BookService.java
@@ -1,4 +1,10 @@
 package com.starlight.app.service;
 
+import com.starlight.app.repository.BooksRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookService {
+    private final BooksRepo repository;
+    public BookService(BooksRepo repository) { this.repository = repository; }
 }
