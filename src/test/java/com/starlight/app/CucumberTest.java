87df3f6be52ed4cb089fd69bf709df83f7e19049
v0teri01 <113454273+v0teri01@users.noreply.github.com>
2025-11-09 17:48:33 -0500

    Update CucumberTest.java

--- src/test/java/com/starlight/app/CucumberTest.java
+++ src/test/java/com/starlight/app/CucumberTest.java
@@ -1,17 +1,40 @@
+// package com.starlight.app;
+
+// import org.junit.platform.suite.api.ConfigurationParameter;
+// import org.junit.platform.suite.api.IncludeEngines;
+// import org.junit.platform.suite.api.SelectClasspathResource;
+// import org.junit.platform.suite.api.Suite;
+
+// @Suite
+// @IncludeEngines("cucumber")
+// @SelectClasspathResource("features") // src/test/resources/features
+// @ConfigurationParameter(key = "cucumber.glue", value = "com.starlight.app.steps")
+// @ConfigurationParameter(
+       //  key = "cucumber.plugin",
+        // value = "pretty,summary,html:target/cucumber-report.html,json:target/cucumber.json,junit:target/cucumber/cucumber.xml"
+)
+// @ConfigurationParameter(key = "cucumber.filter.tags", value = "not @ignored")
+// public class CucumberTest { }
+
+
 package com.starlight.app;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features") // src/test/resources/features
-@ConfigurationParameter(key = "cucumber.glue", value = "com.starlight.app.steps")
-@ConfigurationParameter(
-        key = "cucumber.plugin",
-        value = "pretty,summary,html:target/cucumber-report.html,json:target/cucumber.json,junit:target/cucumber/cucumber.xml"
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = "com.starlight.app.steps",
+    plugin = {
+        "pretty",
+        "summary",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber.json",
+        "junit:target/cucumber/cucumber.xml"
+    },
+    tags = "not @ignored"
 )
-@ConfigurationParameter(key = "cucumber.filter.tags", value = "not @ignored")
-public class CucumberTest { }
+public class CucumberTest {
+}
