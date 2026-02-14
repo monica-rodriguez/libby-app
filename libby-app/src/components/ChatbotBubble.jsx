290710036a85951863c4e36f15e57565d557129b
kelly0003 <kelly.atcher1@gmail.com>
2026-02-14 11:47:28 -0500

    DO NOT MERGE WITH MAIN
    Initial chatbot design:
    imcludes design of window, hover effect,
    and a sample image for the chatbot icon.
    
    Takes user input and displays it in the chat
    window, but does not yet generate responses.

--- libby-app/src/components/ChatbotBubble.jsx
+++ libby-app/src/components/ChatbotBubble.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Chatbot.css";
+import design from "../assets/SF_Chatbot_Design(1).png";
 
 export default function ChatbotBubble({ onClick }) {
   return (
     <div className="chatbot-bubble" onClick={onClick}>
-      ✨
+      <img 
+        src={design} 
+        alt="Chatbot Design" 
+        className="chatbot-image" 
+        style={{ width: "80%", height: "90%" }}
+        />
     </div>
   );
 }
